d7d29e00f2b69f8546d2459b3d2cf12811e16633
Alvaro Delgado <9000816@alumnos.ufv.es>
2022-10-20 21:02:25 +0200

    first commit

--- src/com/pruebaCSV/LectorCSV.java
+++ src/com/pruebaCSV/LectorCSV.java
@@ -2,9 +2,18 @@ package com.pruebaCSV;
 import java.io.FileReader;
 import com.opencsv.CSVReader;
 
-
-
-
-
 public class LectorCSV {
+    public void leeCSV(String nombreArchivo) {
+        try{
+            CSVReader csvReader = new CSVReader(new FileReader(nombreArchivo));
+            String [] fila = null;
+            while ((fila = csvReader.readNext()) != null) {
+                System.out.println(fila[0] + " | " + fila[1] + " | " + fila[2]);
+            }
+            csvReader.close();
+        } catch (Exception ex) {
+            System.out.print(ex.getMessage());
+        }
+
+    }
 }
--- src/com/pruebaCSV/PruebaCSV.java
+++ src/com/pruebaCSV/PruebaCSV.java
@@ -2,7 +2,8 @@ package com.pruebaCSV;
 
 public class PruebaCSV {
     public static void main(String[] args) {
-
+        LectorCSV lector = new LectorCSV();
+        lector.leeCSV("./datos/pokemon.csv");
     }
 }
 
